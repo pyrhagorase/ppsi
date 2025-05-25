2219f7b5ee3e65a1bc096aae3253c2abb24eba4c
Muzza0112 <rahmanlax2004@gmail.com>
2025-05-26 01:16:41 +0700

    edit by Muza - Meperbaiki db user agar bisa di php migrate dan menambahakan akun user, admin dan owner di db:seed

--- sentra_computer/database/seeders/DatabaseSeeder.php
+++ sentra_computer/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,28 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Akun test user biasa
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('user123'), // tambahkan password
+            'role' => 'user',
+        ]);
+
+        // Akun admin
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'), // ganti password sesuai kebutuhan
+            'role' => 'admin',
+        ]);
+
+        // Akun owner
+        User::create([
+            'name' => 'Owner',
+            'email' => 'ownersentra@gmail.com',
+            'password' => Hash::make('owner123'), // ganti password sesuai kebutuhan
+            'role' => 'owner',
         ]);
     }
 }
